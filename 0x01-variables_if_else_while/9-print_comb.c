1d7e5e8234eb5f42e1da94f37e1b9a8bd684f09a
Mverk1 <mver.kurega@gmail.com>
2023-08-14 19:07:13 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 /*
  * File: 9-print_comb.c
- * Auth: Brennan D Baraban
+ * Comfort
  */
 
 #include <stdio.h>
@@ -12,18 +12,19 @@
  */
 int main(void)
 {
-       	int num;
+	int num;
+
 	for (num = 0; num <= 9; num++)
-    {
-        putchar((num % 10) + '0');
-        if (num == 9)
-            continue;
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
 
-        putchar(',');
-        putchar(' ');
-    }
+	putchar('\n');
 
-    putchar('\n');
-
-    return (0);
+	return (0);
 }
