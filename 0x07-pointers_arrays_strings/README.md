2e5bbb1128d712650cc0ebdfc04971b2707f1ee3
Mverk1 <mver.kurega@gmail.com>
2023-08-28 16:52:29 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,11 @@
+0. memset
+	. Write a function that fills memory with a constant byte.
+1. memcp
+
+	.Write a function that copies memory areay
+2. strchr
+
+	.Write a function that locates a character in a string.
+3. strspn
+
+	.Write a function that gets the length of a prefix substring.
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+alfndef MAIN H
+adefine MAIN H
+
+char * menset(char '5, char b, unsigned int n);
+Int _putchar(char c);
+char "
+memcpy (char *dest,
+char "src, unsigned int n);
+char •
+strehr(char 's, char c):
+unsigned int
+strspn(char "s, char "accept);
+char
+strpbrk(char "s, char "accept);
+char
+strstr(char *haystack, char "needle);
+void print chessboard(char (°a)(8));
+vold print_diagsums(int "a, int size);
+void set_string(char **s, char *to);
+
+#endif
+
